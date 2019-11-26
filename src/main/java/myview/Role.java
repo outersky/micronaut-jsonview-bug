b0ce7868b8472f7cc484d36adb7c1d636812df98
outersky <outersky@gmail.com>
2019-11-26 22:11:48 +0800

    @Introspected will conflict with @JsonView

--- src/main/java/myview/Role.java
+++ src/main/java/myview/Role.java
@@ -1,9 +1,12 @@
 package myview;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import io.micronaut.core.annotation.Introspected;
 import lombok.Data;
 
 @Data
+// 加了会导致 Controller 里面的 @JsonView失效
+// @Introspected
 public class Role {
     @JsonView(View.Public.class)
     private Long id;
